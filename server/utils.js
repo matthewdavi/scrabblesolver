5bbeb50ab2414d80d0a7ee6f95f6f32421901f69
matthewdavi <maathewdavis@gmail.com>
2018-09-14 12:42:03 -0400

    utility functions

---
+++ server/utils.js
@@ -0,0 +1,19 @@
+const wordJson = require("./dictionary.json");
+
+const findWords = (word, list) => {
+  const bagContains = (x, y) => Object.keys(y).every(k => x[k] >= y[k]);
+  const letterBag = list => {
+    const x = {};
+    for (const i of list) {
+      x[i] = (x[i] || 0) + 1;
+    }
+    return x;
+  };
+
+  const b = letterBag(word.toUpperCase());
+  return list
+    .filter(w => bagContains(b, letterBag(w.word)))
+    .filter(obj => obj.word !== "")
+    .sort((a, b) => b.score - a.score);
+};
+module.exports = { findWords };
