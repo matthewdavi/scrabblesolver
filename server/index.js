985d224abe255a009fa84f9081a506ae804ea4fe
matthewdavi <maathewdavis@gmail.com>
2018-09-14 14:24:10 -0400

    added cors

--- server/index.js
+++ server/index.js
@@ -5,7 +5,14 @@
  * @param {!express:Response} res HTTP response context.
  */
 const { find } = require("./utils");
-exports.findWords = (req, res) => {
+const cors = require("cors");
+const _findWords = (req, res) => {
   const message = find(req.body.word);
   res.status(200).send(message);
+};
+
+exports.findWords = (req, res) => {
+  cors(req, res, () => {
+    _findWords(req, res);
+  });
 };
