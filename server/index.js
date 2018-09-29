8bc880c6ba46d7dd50836feb18cc430efaea1b08
matthewdavi <maathewdavis@gmail.com>
2018-09-29 12:15:45 -0400

    refactor to use GET instead of POST

--- server/index.js
+++ server/index.js
@@ -6,12 +6,16 @@
  */
 const { find } = require("./utils");
 const _findWords = (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  res.set("Access-Control-Allow-Methods", "GET, POST");
-  const message = find(req.body.word.slice(0, 8));
+  const message = req.query.word
+    ? find(req.query.word)
+    : { word: "error", score: 0 };
   res.status(200).send(message);
 };
 
 exports.findWords = (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+  res.set("Access-Control-Max-Age", "3600");
   _findWords(req, res);
 };
